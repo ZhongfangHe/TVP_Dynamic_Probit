858f4f3308dd0f2aa1a1371bdcdede82f04214ae
Zhongfang He <47869697+ZhongfangHe@users.noreply.github.com>
2022-06-22 11:45:26 -0400

    Update main_simulated_data.m

--- main_simulated_data.m
+++ main_simulated_data.m
@@ -1,64 +1,66 @@
-% Estimate dynamic probit (DProb) and TVP dynamic probit (DProbTVP) models for simulated data
+% Simulate a probit model with TVP:
+% zt = xt * bt + ut, ut ~ N(0,1)
+% bt = btm1 + N(0,diag(v2)), b0 ~ N(0, w)
+% yt = I{zt > 0}
 
 
 clear;
 rng(123456);
-dbstop if warning;
-dbstop if error;
 
-dgp = {'DProb','DProbTVP'}; %data generating process
-ndgp = length(dgp);
+md = {'DPTVP'};
+mj = 1;
+disp(md{mj});
 
-for dgpj = 1:ndgp 
-    for wsj = 1:10 %10 simulation from each dgp
-        % Read data to get y, x
-        read_file = ['Simulated_Data_', dgp{dgpj}, '_IB.xlsx'];
-        read_sheet = ['D',num2str(wsj)];
-        if dgpj == 1 %DProb
-            data = readmatrix(read_file, 'Sheet', read_sheet, 'Range', 'A2:F301');
-        else %DProbTVP
-            data = readmatrix(read_file, 'Sheet', read_sheet, 'Range', 'A2:I301');
+n = 300; %number of data points
+K = 10; %number of regressors
+nrep = 20; %number of simulation samples
+z0 = 0; %initial value of zt
+for dj = 1:nrep
+    x = [ones(n,1)  randn(n,K-1)]; %regressors
+    eta = randn(n,1); %innovations of zt
+    
+    beta_true = zeros(n,K); %first coef (intercept) is a constant, second coef is TVP, others zero
+    beta_true(:,1) = -0.2;
+    beta0_true = 0;
+    vtrue = 0.1; 
+    for t = 1:n
+        if t > 1
+            beta_true(t,2) = beta_true(t-1,2) + vtrue * randn;
+        else
+            beta_true(t,2) = beta0_true + vtrue * randn;
         end
-        y = data(:,1);
-        x = data(:,2:4);
-        p1true = data(:,6);
-
-        % Setup of estimation
-        ind_sparse = 0;
-        ind_pred = 0;
-
-        burnin = 1000*2;
-        ndraws = 5000*2;
-        disp(['burnin = ', num2str(burnin), ', ndraws = ', num2str(ndraws)]);
-
-        % DProb HS
-        draws = Est_DProb_HS(y, x, burnin, ndraws, ind_sparse, ind_pred);
-        tmp = draws.p1 - repmat(p1true',ndraws,1); 
-        rmse_dprob = sqrt(mean(tmp.^2))';
-        xlswrite(read_file, rmse_dprob, read_sheet, 'K2');
-        
-        % DProb SS
-        draws = Est_DProb_SS(y, x, burnin, ndraws, ind_sparse, ind_pred);
-        tmp = draws.p1 - repmat(p1true',ndraws,1); 
-        rmse_dprob = sqrt(mean(tmp.^2))';
-        xlswrite(read_file, rmse_dprob, read_sheet, 'L2');        
-        
-        % DProbTVP HS
-        draws = Est_DProbTVP_HS(y, x, burnin, ndraws, ind_sparse, ind_pred);
-        tmp = draws.p1 - repmat(p1true',ndraws,1); 
-        rmse_dprob = sqrt(mean(tmp.^2))';
-        xlswrite(read_file, rmse_dprob, read_sheet, 'M2');
-        
-        % DProbTVP SS
-        draws = Est_DProbTVP_SS(y, x, burnin, ndraws, ind_sparse, ind_pred);
-        tmp = draws.p1 - repmat(p1true',ndraws,1); 
-        rmse_dprob = sqrt(mean(tmp.^2))';
-        xlswrite(read_file, rmse_dprob, read_sheet, 'N2');        
     end
+    xb = sum(x .* beta_true,2);
+
+    phi = 0.8; %AR coef
+    ztrue = zeros(n,1);
+    p1true = zeros(n,1);
+    y = zeros(n,1);
+    for t = 1:n
+        if t > 2
+            zt_mean = phi * ztrue(t-1) + xb(t);
+        else
+            zt_mean = phi * z0 + xb(t);
+        end
+        zt = zt_mean + eta(t);
+        ztrue(t) = zt;
+        p1true(t) = normcdf(zt_mean); 
+        y(t) = (zt > 0);    
+    end
+    disp(['rep ', num2str(dj),': mean(y) = ', num2str(mean(y))]);
+
+    write_file = ['SimData_',md{mj},'.xlsx'];
+    write_sheet = ['D',num2str(dj)];
+    xstr = cell(1,K);
+    betastr = cell(1,K);
+    for j = 1:K
+        xstr{j} = ['x',num2str(j)];
+        betastr{j} = ['beta',num2str(j)];
+    end
+    title = [{'y'} xstr {'z','p1'} betastr];
+    writecell(title, write_file, 'Sheet', write_sheet, 'Range', 'A1');
+    writematrix([y x ztrue p1true  beta_true], write_file, 'Sheet', write_sheet, 'Range', 'A2');
 end
-
-
-
 
 
 
